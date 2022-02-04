c8c05c00c86c8bedc67ecc41232f5ddf1967e925
Justin Otter <cv-12578@cvpost019.cv.nrao.edu>
2022-02-04 13:51:24 -0500

    fixing up imaging scripts. Continuum running with spw0

--- clean_all_cubes.py
+++ clean_all_cubes.py
@@ -1,9 +1,23 @@
 # coding: utf-8
 
 import os
-import csv
-        
+import csv 
 
+#spw = os.getenv('SPW')
+spw = '0'
+
+if spw == '0':
+    mol = 'CO'
+    restfreq = "115.2601925GHz"
+if spw == '1':
+    mol = 'CN'
+    restfreq = "113.5GHz"
+if spw == '2':
+    mol = 'CH3OH'
+    restfreq = "107.013831GHz"
+if spw == '3':
+    mol = 'HC3N'
+    restfreq = "100.076392GHz" #v=0, J=11-10
 
 plifu_list = []
 chan_lower = []
@@ -18,58 +32,77 @@ with open('listobs_params.csv', mode='r') as fl:
             chan_upper.append(lines[-2])
             chan_lower.append(lines[-1])
 
-#CO rest freq
-restfreq = "115.271202GHz"
-
 theoutframe = 'LSRK'
 theveltype = 'radio'
 restoringbeam = ['2.5arcsec']
+#restoringbeam = 'common'
+chanchunks = -1
 niter = 100000
 robust_value = 0.5
+nterms = 1
 thecellsize = '0.5arcsec'
 theimsize=[256,256]
+velwidth='11km/s'
 
+
+contsub_list = ['8080-3704', '8083-12703', '8982-6104', '9088-9102', '9194-3702', '9494-3702']
+contsub = True
 
 import_pipe3d = False
 
-for ind, plateifu in enumerate(plifu_list): #skipping first bc I tested w that galaxy
-    print('BEGINNING IMAGING FOR '+plateifu)
+for ind,plateifu in enumerate(plifu_list[:1]):
+
+    print('BEGINNING IMAGING FOR '+plateifu+' SPW '+spw)
     datadir = '/lustre/cv/observers/cv-12578/data/'
-    savedir = datadir+'data_products/target'+plateifu+'/cont_imaging/'
+    savedir = datadir+'data_products/target'+plateifu+'/spw'+spw+'_cube/'
     fitsdir = datadir+'data_products/fitsimages/'
     msdir = datadir+'split_ms/'
     pipe3ddir = datadir+'pipe3d/'
 
     if not os.path.isdir(savedir):
         os.mkdir(savedir)
+
     os.chdir(datadir)
     
-    fullvis = msdir+'target'+plateifu+'_vis.ms'
+    fullvis = 'target'+plateifu+'_vis.ms'
     if plateifu == '8081-3702' or plateifu == '9088-9102' or plateifu == '9494-3701':
-        fullvis = msdir+'target'+plateifu+'_concat_vis.ms'
+        fullvis = 'target'+plateifu+'_concat_vis.ms'
 
-    spwrange = '1,2,3,0:0~'+chan_lower[ind]+';'+chan_upper[ind]+'~1920'
-    #spwrange= '1,2,3'
+    imgname = 'target'+plateifu+'_cube_spw'+spw+'_'+mol+'_r'+str(robust_value)
+    
+    #continuum subtraction only for those that need it
+    if plateifu in contsub_list:
+        cont_spw = '1,2,3,0:0~'+chan_lower[ind]+';'+chan_upper[ind]+'~1920'
+        if contsub == True:
+            uvcontsub(vis=fullvis,
+                      spw='0,1,2,3',
+                      fitspw=cont_spw,
+                      excludechans=False,
+                      combine='spw',
+                      solint='int',
+                      fitorder=0,
+                      want_cont=False) 
 
+        imgname = 'target'+plateifu+'_cube_spw'+spw+'_'+mol+'_r'+str(robust_value)+'_contsub'
 
-    imgname = 'target'+plateifu+'_cont_spw0123_r'+str(robust_value)#+'_exchans:'+lower_chan+'-'+upper_chan
     d_imgname = imgname+'.dirty'
 
     #creating dirty cube
     tclean(vis=fullvis,
              imagename=savedir+d_imgname,
              field=plateifu,
-             spw=spwrange,
+             spw=spw,
              restfreq=restfreq,
              threshold='1Jy', # high threshold for dirty cube  
              imsize=theimsize,  # we used [256,256], just to make sure there is no weird features outside of the galaxy
              cell=thecellsize,
              outframe=theoutframe,
              restoringbeam=restoringbeam,  # to match MaNGA resolution,  # “common” will give you the native beam size of this observation, use restoringbeam=[’2.5arcsec’] to generate a map with resolution similar to that of MaNGA. 
-             specmode='mfs',
+             specmode='cube',
              gridder='standard',
+             width=velwidth,
              veltype=theveltype,
-             niter=0, # 0 for dirty cube 
+             niter=0, # 0 for dirty cube
              pbcor=False,
              deconvolver='hogbom',
              weighting = 'briggs', #'briggs' or 'natural' or 'uniform' 
@@ -79,34 +112,35 @@ for ind, plateifu in enumerate(plifu_list): #skipping first bc I tested w that g
     exportfits(imagename=savedir+d_imgname+'.image',
                fitsimage=fitsdir+d_imgname+'.fits',
                overwrite=True)
-    
+
     imstats = imstat(savedir+d_imgname+'.image')
     imgrms = imstats['rms'][0]
     str_rms = str(round(imgrms, 5))
     c_imgname = imgname+'_'+str_rms+'Jy'
-    
+
     tclean(vis=fullvis,
            imagename=savedir+c_imgname,
            field=plateifu,
-           spw=spwrange,
+           spw=spw,
            restfreq=restfreq,
-           threshold=str(imgrms)+'Jy', # rms of cube, we clean down to 1sigma 
-           imsize=theimsize,  # we used [256,256], just to make sure there is no weird features outside of the galaxy
+           threshold=str(imgrms)+'Jy', # rms of cube, we clean down to 1sigma
+           imsize=theimsize,  # we used [256,256], just to make sure there is no weird features outside of the galaxy                               
            cell=thecellsize,
            outframe=theoutframe,
-           restoringbeam=restoringbeam,                                                                               
-           specmode='mfs',
+           restoringbeam=restoringbeam,
+           specmode='cube',
            gridder='standard',
+           width=velwidth,
            veltype=theveltype,
-           niter=niter, # we use 2000 or so, a large number anyway.
+           niter=niter,
            pbcor=False,
            deconvolver='hogbom',
-           weighting = 'briggs', #'briggs' or 'natural' or 'uniform'  
+           weighting = 'briggs', #'briggs' or 'natural' or 'uniform'       
            robust = robust_value,
            interactive=False)
 
     exportfits(imagename=savedir+c_imgname+'.image',
-                fitsimage=fitsdir+c_imgname+'.img.fits',
+                fitsimage=fitsdir+c_imgname+'.cube.fits',
                 overwrite=True)
 
     c_imgname_pbcor = c_imgname+'.pbcor'
@@ -117,20 +151,19 @@ for ind, plateifu in enumerate(plifu_list): #skipping first bc I tested w that g
             overwrite=True)
 
     exportfits(imagename=savedir+c_imgname_pbcor+'.image',
-                fitsimage=fitsdir+c_imgname_pbcor+'.img.fits',
+                fitsimage=fitsdir+c_imgname_pbcor+'.cube.fits',
                 overwrite=True)
 
-    # Regrid the cube to MaNGA grid (we used H-alpha map from Pipe3D as the template. The fits header of Pipe3D map was not completed, Hsi-An had to fix the header first before using it as a template.   
+    # Regrid the cube to MaNGA grid (we used H-alpha map from Pipe3D as the template. The fits header of Pipe3D map was not completed, Hsi-An had to fix the header first before using it as a template.
     if import_pipe3d == True:
         importfits(fitsimage=pipe3ddir+'manga-'+plateifu+'.Pipe3D.cube.edit.fits', imagename=savedir+'manga-'+plateifu+'.Pipe3D.cube.edit.image')
-    
+
     imregrid(imagename=savedir+c_imgname_pbcor+'.image',
              output=savedir+c_imgname_pbcor+'.regrid.image',
              template=savedir+'manga-'+plateifu+'.Pipe3D.cube.edit.image',axes=[0,1],
              overwrite=True)
-    
+
 
     exportfits(imagename=savedir+c_imgname_pbcor+'.regrid.image',
-               fitsimage=fitsdir+c_imgname_pbcor+'.regrid.img.fits',
+               fitsimage=fitsdir+c_imgname_pbcor+'.regrid.cube.fits',
                overwrite=True)
-     
